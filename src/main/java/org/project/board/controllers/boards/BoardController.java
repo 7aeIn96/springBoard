f2e207a2eb831369a5f1aca1b580e7d8f9bc8118
7aeIn96 <xodls713@naver.com>
2023-06-01 18:24:39 +0900

    게시판 작성 양식 완료

--- src/main/java/org/project/board/controllers/boards/BoardController.java
+++ src/main/java/org/project/board/controllers/boards/BoardController.java
@@ -1,19 +1,28 @@
 package org.project.board.controllers.boards;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.project.board.controllers.admins.BoardForm;
+import org.project.board.commons.CommonException;
+import org.project.board.commons.MemberUtil;
 import org.project.board.entities.Board;
+import org.project.board.models.board.config.BoardConfigInfoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 @RequiredArgsConstructor
 public class BoardController {
+    private final BoardConfigInfoService boardConfigInfoService;
+    private final HttpServletResponse response;
+    private final MemberUtil memberUtil;
+    private Board board; // 게시판 설정
+
     /**
      * 게시글 목록
      * @param bId
@@ -21,6 +30,7 @@ public class BoardController {
      */
     @GetMapping("/list/{bId}")
     public String list(@PathVariable String bId, Model model) {
+        commonProcess(bId, "list", model);
 
         return "board/list";
     }
@@ -32,7 +42,8 @@ public class BoardController {
      * @return
      */
     @GetMapping("/write/{bId}")
-    public String write(@PathVariable String bId) {
+    public String write(@PathVariable String bId, Model model) {
+        commonProcess(bId, "write", model);
 
         return "board/write";
     }
@@ -43,14 +54,70 @@ public class BoardController {
      * @return
      */
     @GetMapping("/{id}/update")
-    public String update(@PathVariable Long id) {
+    public String update(@PathVariable Long id, Model model) {
+        commonProcess(null, "update", model);
 
         return "board/update";
     }
 
     @GetMapping("/view/{id}")
-    public String view(@PathVariable Long id) {
+    public String view(@PathVariable Long id, Model model) {
+        commonProcess(null, "view", model);
 
         return "board/view";
+    }
+
+    @PostMapping("/save")
+    public String save() {
+
+        return null;
+    }
+
+    private void commonProcess(String bId, String action, Model model) {
+        /**
+         * 1. bId 게시판 설정 조회
+         * 2. action - write, update - 공통 스크립트, 공통 CSS
+         *           - 에디터 사용 -> 에디터 스크립트 추가
+         *           - 에디터 미사용 -> 에디터 스크립트 미추가
+         *           - write, list, view -> 권한 체크
+         *           - update - 본인이 게시글만 수정 가능
+         *                    - 회원 - 회원번호
+         *                    - 비회원 - 비회원비밀번호
+         *                    - 관리자는 다 가능
+         *
+         */
+        board = boardConfigInfoService.get(bId, action);
+        List<String> addCss = new ArrayList<>();
+        List<String> addScript = new ArrayList<>();
+
+        // 공통 스타일 CSS
+        addCss.add("board/style");
+        addCss.add(String.format("board/%s_style", board.getSkin())); // 스킨에 따른 스타일 적용 가능
+
+        // 글 작성, 수정시 필요한 자바스크립트
+        if (action.equals("write") || action.equals("update")) {
+            if (board.isUseEditor()) { // 에디터 사용 경우
+                addScript.add("ckeditor/ckeditor");
+            }
+            addScript.add("board/form");
+        }
+
+        // 공통 필요 속성 추가
+        model.addAttribute("board", board); // 게시판 설정
+        model.addAttribute("addCss", addCss); // CSS 설정
+        model.addAttribute("addScript", addScript); // JS 설정
+    }
+
+    @ExceptionHandler(CommonException.class)
+    public String errorHandler(CommonException e, Model model) {
+        e.printStackTrace();
+
+        String message = e.getMessage();
+        HttpStatus status = e.getStatus();
+        response.setStatus(status.value());
+
+        String script = String.format("alert('%s');history.back();", message);
+        model.addAttribute("script", script);
+        return "commons/execute_script";
     }
 }
---
+++ src/test/java/org/project/board/tests/BoardSaveTests.java
@@ -0,0 +1,4 @@
+package org.project.board.tests;
+
+public class BoardSaveTests {
+}
